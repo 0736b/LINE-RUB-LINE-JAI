89ce6747f80e22fb18514dc2bf8c34aaecc26f49
0736b <thanawat_bumpengpun@cmu.ac.th>
2022-12-28 03:41:36 +0700

    feat: sheet format color

--- sheet_manager.py
+++ sheet_manager.py
@@ -26,12 +26,14 @@ class SheetManager:
     def append_data(self, date_time, command, price, description):
         records = self.sheet.get_all_records()
         len_records = len(records)
-        if len(records) == 0:
+        if len_records == 0:
             match command:
                 case "รับ":
                     no = 1
                     balance = price
-                    row_data = [no, str(date_time), str(command), float(price), str(description), float(balance)]
+                    diff = "+" + str(float(price))
+                    row_data = [no, str(date_time), str(command), float(price), str(description), float(balance), str(diff)]
+                    self.format_color(self.start_index, command)
                     self.sheet.insert_row(row_data, self.start_index)
                     return "บันทึกรายการที่ " + str(no) + "สำเร็จ"
                 case "จ่าย":
@@ -41,17 +43,41 @@ class SheetManager:
             no = len_records + 1
             index = self.start_index + len_records
             balance = float(last_record['ยอดคงเหลือ'])
+            diff = "0"
             match command:
                 case "รับ":
                     balance += float(price)
+                    diff = "+" + str(float(price))
                 case "จ่าย":
                     balance -= float(price)
-            row_data = [no, str(date_time), str(command), float(price), str(description), float(balance)]
+                    diff = "-" + str(float(price))
+            row_data = [no, str(date_time), str(command), float(price), str(description), float(balance), str(diff)]
+            self.format_color(index, command)
             self.sheet.insert_row(row_data, index)
             return "บันทึกรายการที่ " + str(no) + "สำเร็จ"
-
+        
+    def format_color(self, index, command):
+        text_center_cell_range = "A$:G$".replace("$", str(index))
+        command_cell_range = "C$".replace("$", str(index))
+        diff_cell_range = "G$".replace("$", str(index))
+        self.sheet.format(text_center_cell_range, {"backgroundColor": {"red": 0.0, "green": 0.0, "blue": 0.0}, "horizontalAlignment": "CENTER", "textFormat": {"fontSize": 10, "foregroundColor": {"red": 1.0, "green": 1.0, "blue": 1.0}}})
+        match command:
+            case "รับ":
+                self.sheet.format(command_cell_range, {"backgroundColor": {"red": 0.0, "green": 0.0, "blue": 0.0}, "textFormat": {"foregroundColor": {"red": 0.0, "green": 1.0, "blue": 0.0}}})
+                self.sheet.format(diff_cell_range, {"backgroundColor": {"red": 0.0, "green": 0.0, "blue": 0.0}, "textFormat": {"foregroundColor": {"red": 0.0, "green": 1.0, "blue": 0.0}}})
+            case "จ่าย":
+                self.sheet.format(command_cell_range, {"backgroundColor": {"red": 0.0, "green": 0.0, "blue": 0.0}, "textFormat": {"foregroundColor": {"red": 1.0, "green": 0.0, "blue": 0.0}}})
+                self.sheet.format(diff_cell_range, {"backgroundColor": {"red": 0.0, "green": 0.0, "blue": 0.0}, "textFormat": {"foregroundColor": {"red": 1.0, "green": 0.0, "blue": 0.0}}})
+        
+        
 if __name__ == "__main__":
     sheet_mgr = SheetManager()
     sheet_mgr.connect()
-    status = sheet_mgr.append_data("2022-12-28 02:10:27", "จ่าย", 9000.0, "เงินรายเดือน")
+    status = sheet_mgr.append_data("2022-12-28 01:10:27", "รับ", 6000.0, "เงินรายเดือน")
+    print(status)
+    status = sheet_mgr.append_data("2022-12-28 02:10:27", "จ่าย", 60.0, "กะเพราหมู")
+    print(status)
+    status = sheet_mgr.append_data("2022-12-28 03:10:27", "จ่าย", 103.75, "กุ้งแก้ว")
+    print(status)
+    status = sheet_mgr.append_data("2022-12-28 04:10:27", "รับ", 1000.0, "หวย")
     print(status)
